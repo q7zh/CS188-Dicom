5b2adf4c599c1cd281aa29732bf4281fae231a43
q7zh <qiliang@ucla.edu>
2018-11-08 22:34:40 -0800

    add tip page

--- javascript/tip.js
+++ javascript/tip.js
@@ -35,6 +35,17 @@ $(document).ready(function(){
     }
     
   });
+
+  $(".segmented label input[type=radio]").each(function(){
+    	$(this).on("change", function() {
+        	if($(this).is(":checked")){
+           		$(this).parent().siblings().each(function(){
+                	$(this).removeClass("checked");
+            	});
+            	$(this).parent().addClass("checked");
+        	}
+    	});
+	});
   
   
 });
